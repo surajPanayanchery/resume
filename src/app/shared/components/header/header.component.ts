c0bfa46f7381a93cc981e56581d2f0f93b4f971f
surajPanayanchery <suraj.panayanchery@gmail.com>
2021-03-03 09:37:55 +0530

    styles changes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
 import { faBalanceScale } from '@fortawesome/free-solid-svg-icons/faBalanceScale';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons/faBriefcase';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
 import { faCaretUp } from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons/faGraduationCap';
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
 import { faTasks } from '@fortawesome/free-solid-svg-icons/faTasks';
 import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
 
@@ -16,6 +20,12 @@ export class HeaderComponent implements OnInit {
   arrowDown = faCaretDown;
   arrowUp = faCaretUp;
   counter = faBalanceScale;
+  home = faHome;
+  work = faBriefcase;
+  education = faGraduationCap;
+  snippets = faCode
+
+
   constructor() {}
 
   ngOnInit(): void {}
